caa17d06266c063cba53af57f4ccf9a203cd5a9f
Guy Royse <guy@guyroyse.com>
2020-05-15 16:04:52 -0400

    pretty pages

--- js/server.js
+++ js/server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(express.static('static'))
 
 const AbilityGenerator = require('./src/ability-generator')
 let abilityGenerator = new AbilityGenerator()
